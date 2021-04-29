15913d1db91c6148b2b3e1d5d02f4078db53163a
Severine <simone.yvonne.baudry@gmail.com>
2021-04-29 17:10:54 +0200

    add previous data if needed to compute daily data

--- airflow/dags/dag_psql.py
+++ airflow/dags/dag_psql.py
@@ -21,11 +21,11 @@ with DAG(
     tags = ["covid", "psql"],
     ) as dag:
     
-    create_temp_county_table = PostgresOperator(
-        task_id="create_temp_county_table",
+    create_recent_per_county_table = PostgresOperator(
+        task_id="create_recent_per_county_table",
         postgres_conn_id="postgres_default",
         sql="""
-            CREATE TABLE IF NOT EXISTS temp_county (
+            CREATE TABLE IF NOT EXISTS recent_per_county (
                 date date,
                 county text,
                 state text,
@@ -36,60 +36,110 @@ with DAG(
                 """,
         )
     
-    load_temp_county_table = PostgresOperator(
-        task_id = "load_temp_county_table",
+    load_recent_per_county_table = PostgresOperator(
+        task_id = "load_recent_per_county_table",
         postgres_conn_id = "postgres_default",
         sql = """
-            COPY temp_county FROM '/tmp/us-counties-recent.csv' WITH CSV HEADER ;
+            COPY recent_per_county FROM '/tmp/us-counties-recent.csv' WITH CSV HEADER ;
         """
         )
-    filter_date = PostgresOperator(
-        task_id = "filter_date",
+    # add location_id key to new data
+    add_location_id = PostgresOperator(
+        task_id = "add_location_id",
         postgres_conn_id = "postgres_default",
-        sql = """
-            DELETE FROM temp_county WHERE date > ( DATE '2021-04-10') ;
+        sql =  """
+            ALTER TABLE recent_per_county ADD COLUMN location_id bigint;
+            UPDATE recent_per_county AS new
+            SET location_id = loc.location_id
+            FROM nyt_locations_geography AS loc
+            WHERE ( (loc.county = new.county) AND (loc.state = new.state) AND ( (loc.fips = new.fips) OR ( loc.fips Is NULL AND new.fips IS NULL) ) );
+            """
+            )
+    # to compute daily data from cumulated data, it is necessary to have the previous cumulated data
+    # for some counties with very few cases / deaths, previous data does not appear in the 1-month window
+    # we must thus retrieve it from past data and add it to recent data
+    # this is done in 2 phases :
+    #   - First, get min(date) in recent data for each location_id (table min_recent)
+    #   - Then, compare with max(date) for past_data : 
+    #       - if min(recent) > max(past) : add max(past) for location_id to recent table 
+    retrieve_past_data = PostgresOperator(
+        task_id = "retrieve_past_data",
+        postgres_conn_id = "postgres_default",
+        sql =  """
+            WITH min_recent AS 
+            ( WITH t0 AS 
+                (SELECT *, rank() OVER (PARTITION BY location_id ORDER BY date) FROM recent_per_county)
+                SELECT * FROM t0  WHERE rank = 1
+            )
+            INSERT INTO recent_per_county(location_id, state, county, fips, date, cases, deaths)
+            SELECT  last_data.location_id, last_data.state, last_data.county, last_data.fips, last_data.date, last_data.cases, last_data.deaths 
+            FROM last_data JOIN min_recent 
+            ON last_data.location_id = min_recent.location_id
+            WHERE last_data.date < min_recent.date;        
         """
-        )
+        )   
     
-    filter_locations = PostgresOperator(
-        task_id = "filter_location",
-        postgres_conn_id = "postgres_default",
-        sql = """
-             CREATE TABLE temp_county_2 AS
-            (   SELECT date, location_id, deaths, cases
-                FROM temp_county AS n JOIN nyt_locations_geography as l
-                ON  (l.county = n.county) AND (l.state = n.state) AND ( (l.fips = n.fips) OR ( l.fips Is NULL AND n.fips IS NULL) )
-            
-            );
-
-        """
-        )
+    # compute daily deaths and cases from cumulated values :
+    # - partition by location_id and order by date
+    # - compute previous cases and deaths by "shifting" the case and deaths column
+    # - compute daily data : cumulated_current - cumulated_prev
     compute_daily_stats = PostgresOperator(
         task_id = "compute_daily_stats",
         postgres_conn_id = "postgres_default",
         sql = """
-            CREATE TABLE temp_daily AS
-            WITH zuzu AS (SELECT location_id, date, deaths, lag(deaths) OVER w AS deaths_prev, cases, lag(cases) OVER w AS cases_prev 
-                FROM temp_county_2 
-                WINDOW w AS (PARTITION BY location_id ORDER BY date ASC)  )
-            SELECT location_id, deaths, deaths_prev, deaths-deaths_prev AS daily_deaths, cases, cases_prev, cases - cases_prev AS daily_cases FROM zuzu;
-            """
+            ALTER TABLE recent_per_county 
+            ADD COLUMN daily_cases int,
+            ADD COLUMN daily_deaths int ;
+            WITH prev AS 
+            (SELECT location_id, date, lag(deaths) OVER w AS deaths_prev, lag(cases) OVER w AS cases_prev 
+                FROM recent_per_county 
+                WINDOW w AS (PARTITION BY location_id ORDER BY date ASC)  
             )
+            UPDATE recent_per_county AS new
+            SET daily_cases = new.cases - prev.cases_prev , daily_deaths = new.deaths - prev.deaths_prev
+            FROM prev AS prev
+            WHERE new.date = prev.date AND new.location_id = prev.location_id;
+             """
+            )
+             
+            
+    #filter_date = PostgresOperator(
+        #task_id = "filter_date",
+        #postgres_conn_id = "postgres_default",
+        #sql = """
+            #DELETE FROM recent_per_county WHERE date > ( DATE '2021-04-10') ;
+        #"""
+        #)
     
-    drop_temp_county_table = PostgresOperator(
-        task_id = "drop_temp_county_table",
+    #filter_locations = PostgresOperator(
+        #task_id = "filter_location",
+        #postgres_conn_id = "postgres_default",
+        #sql = """
+             #CREATE TABLE recent_per_county_2 AS
+            #(   SELECT date, location_id, deaths, cases
+                #FROM recent_per_county AS n JOIN nyt_locations_geography as l
+                #ON  (l.county = n.county) AND (l.state = n.state) AND ( (l.fips = n.fips) OR ( l.fips Is NULL AND n.fips IS NULL) )
+            
+            #);
+
+        #"""
+        #)
+    
+    drop_recent_per_county_table = PostgresOperator(
+        task_id = "drop_recent_per_county_table",
         postgres_conn_id = "postgres_default",
         sql = """
-        DROP TABLE IF EXISTS temp_county;
+        DROP TABLE IF EXISTS recent_per_county;
         """
         )
-    drop_temp_county_2_table = PostgresOperator(
+    drop_recent_per_county_2_table = PostgresOperator(
         task_id = "drop_temp_county_2_table",
         postgres_conn_id = "postgres_default",
         sql = """
-        DROP TABLE IF EXISTS temp_county_2;
+        DROP TABLE IF EXISTS recent_per_county_2;
         """
         )
     
     
-[ drop_temp_county_table, drop_temp_county_2_table] >> create_temp_county_table >> load_temp_county_table >> filter_date >> filter_locations 
+[ drop_recent_per_county_table, drop_recent_per_county_2_table] >> create_recent_per_county_table >> load_recent_per_county_table >> add_location_id >> retrieve_past_data >> compute_daily_stats
+#filter_date >> filter_locations 
