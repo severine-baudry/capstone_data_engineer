75150b9ce226f90e4b61d32b3a392f301f357793
Severine <simone.yvonne.baudry@gmail.com>
2021-05-04 13:26:18 +0200

    stage recent weather tables in airflow dag

--- airflow/dags/weather_dag.py
+++ airflow/dags/weather_dag.py
@@ -25,8 +25,6 @@ import logging
 
 args = {
     'owner': 'Airflow',
-    'schedule_interval' : '@daily',
-
 }
 
 class download_diff_weather(BaseOperator):
@@ -107,15 +105,17 @@ class dummy_download( BaseOperator):
         self.log.info( f"I AM {self.__class__.__name__}" )
         yesterday = context["yesterday_ds"]
         day_1 = ds_add(yesterday, -1)
-        self.log.info( f"AVANT HIER {day_1}" )
+        end = ds_add(yesterday, 0)
+        self.log.info( f"yesterday {yesterday}" )
+        self.log.info( f"END pouet {end}" )
         for i in range(1, self.max_days_depth):
-            begin = ds_add(yesterday, -i)
-            url_file = self.url_prefix +f"{ ds_format(begin, '%Y-%m-%d', '%Y%m%d')}_to_{ ds_format(yesterday, '%Y-%m-%d', '%Y%m%d')}.tar.gz"
+            begin = ds_add(end, -i)
+            url_file = self.url_prefix +f"{ ds_format(begin, '%Y-%m-%d', '%Y%m%d')}_to_{ ds_format(end, '%Y-%m-%d', '%Y%m%d')}.tar.gz"
             out = os.path.join(self.out_dir, url_file)
             if os.path.isfile(out) :
                 self.log.info(f"{out} has been donwloaded !!")
                 context["task_instance"].xcom_push(key = "first_date", value = begin)
-                context["task_instance"].xcom_push(key = "last_date", value = yesterday)
+                context["task_instance"].xcom_push(key = "last_date", value = end)
                 tar = tarfile.open(out, "r:*")
                 tar_root = os.path.commonprefix(tar.getnames())
                 untar_dir = os.path.join("/tmp/", tar_root)
@@ -136,11 +136,41 @@ class dummy_download( BaseOperator):
             self.log.error(f"downloading diff weather files failed : try to increase {self.max_days_depth}")
             raise(Exception( "downloading diff weather files failed") )
 
+sql_filter = """
+            DROP TABLE IF EXISTS {{params.filtered_table}};
+            CREATE TABLE {{params.filtered_table}} AS
+            WITH temp AS (
+                SELECT * FROM {{params.full_table}}
+                WHERE quality_flag IS NULL
+                )
+            SELECT temp.measured, temp.station_id,  temp.date, temp.value 
+            FROM temp JOIN {{params.selected_stations}}  AS sel
+            ON sel.station_id = temp.station_id AND sel.measured = temp.measured
+
+            """
+            
+sql_filter_v1 = """
+            DROP TABLE IF EXISTS {{params.filtered_table}};
+            CREATE TABLE {{params.filtered_table}} AS
+            WITH temp AS (
+                SELECT * FROM {{params.full_table}}
+                WHERE quality_flag IS NULL
+                )
+            SELECT * 
+            FROM temp JOIN {{params.selected_stations}}  AS sel
+            ON sel.location_id = temp.location_id AND sel.date = temp.date
+            
+            """
+sql_filter_v0 = """
+            SELECT * FROM {{params.table}}
+            """
 with DAG(
-    dag_id='daily_weather',
+    dag_id='daily_weather_2',
     default_args=args,
-    schedule_interval='@daily',
+    schedule_interval= '@once', #'@monthly', # for testing #  @daily',
     start_date= datetime(2021,5,2), #days_ago(2), #datetime.datetime.now(), #days_ago(2),
+#    end_date= datetime(2021,5,2), #days_ago(2), #datetime.datetime.now(), #days_ago(2),
+    max_active_runs = 1,
     tags=['covid'],
 ) as dag:
     
@@ -153,20 +183,46 @@ with DAG(
             sql="""
             DROP TABLE IF EXISTS recent_insert;
             CREATE TABLE IF NOT EXISTS recent_insert (LIKE weather);
+            
             COPY recent_insert FROM '"""  +
             "{{ti.xcom_pull(key='weather_diff_dir')}}"  + 
-            """/insert.csv' WITH CSV HEADER;            
+            """/insert.csv' WITH CSV HEADER; 
             """            
             )
-#            SELECT {{ params.insert_file}};
-#            SELECT {{ti.xcom_pull( key = \'weather_diff_dir\', task_ids= [\'download_diff_weather\'])}};
-
-#            params = { "insert_file" : os.path.join( "{{ ti.xcom_pull(task_ids= ['download_diff_weather'], key = 'weather_diff_dir')}}", "insert.csv") }
-#              params = { "insert_file" : "'{{ti.xcom_pull( key = \'weather_diff_dir\', task_ids= [\'download_diff_weather\'])}}'" }
-#            params = { "insert_file" : {{xcom_pull( key = 'weather_diff_dir', task_ids= ['download_diff_weather'])}} }
-#             params = { "insert_file" : "toto"}
-#             "{{ ti.xcom_pull( key = 'weather_diff_dir', task_ids= ['download_diff_weather']) }} "  + "'";
-      
+        stage_recent_delete = PostgresOperator(
+            task_id = "stage_recent_delete",
+            postgres_conn_id = "postgres_default",
+            sql="""
+            DROP TABLE IF EXISTS recent_delete;
+            CREATE TABLE IF NOT EXISTS recent_delete (LIKE weather);
+            COPY recent_delete FROM '"""  +
+            "{{ti.xcom_pull(key='weather_diff_dir')}}"  + 
+            """/delete.csv' WITH CSV HEADER;            
+            """            
+            )
+        stage_recent_update = PostgresOperator(
+            task_id = "stage_recent_update",
+            postgres_conn_id = "postgres_default",
+            sql="""
+            DROP TABLE IF EXISTS recent_update;
+            CREATE TABLE IF NOT EXISTS recent_update (LIKE weather);
+            COPY recent_update FROM '"""  +
+            "{{ti.xcom_pull(key='weather_diff_dir')}}"  + 
+            """/update.csv' WITH CSV HEADER;            
+            """            
+            )
+        
+        filter_insert = PostgresOperator(
+            task_id = "filter_insert",
+            postgres_conn_id = "postgres_default",
+            sql = sql_filter,
+            params= {"full_table" : "recent_insert", 
+                     "filtered_table" : "recent_insert_filtered",
+                     "selected_stations" : "map_locations_stations"}
+            )
+ #           parameters = {'filtered_table' : "recent_insert_filtered",
+ #                         'stage_table' : "recent_insert"}
+        
         #filter_quality_check = PostgresOperator(
             #task_id = "filter_quality_check",
             #postgres_conn_id = "postgres_default",
@@ -175,4 +231,5 @@ with DAG(
             #)
         
     
-download_diff_weather_task >> stage_recent_insert
+download_diff_weather_task >> [stage_recent_insert, stage_recent_update, stage_recent_delete]
+stage_recent_insert >> filter_insert
