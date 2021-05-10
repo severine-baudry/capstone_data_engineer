b4c6ebe44692d88dc57763559a57b50b4fa5c318
Severine <simone.yvonne.baudry@gmail.com>
2021-05-10 15:41:39 +0200

    clean code : remove function to download weather data, now in weather_dag

--- airflow/dags/covid_dag.py
+++ airflow/dags/covid_dag.py
@@ -56,104 +56,7 @@ class download_fromweb(BaseOperator):
         #return url
     
 
-class download_diff_weather(BaseOperator):
-    #template_fields = ["url_prefix"]
-    @apply_defaults
-    def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 url_dir = "https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/superghcnd/",
-                 url_prefix ="superghcnd_diff_",
-                 out_dir="/tmp/",
-                 max_days_depth = 5,
-                 *args, **kwargs):
-
-        super(download_diff_weather, self).__init__(*args, **kwargs)
-        # Map params here
-        self.url_dir = url_dir
-        self.url_prefix = url_prefix
-        self.out_dir = out_dir
-        self.max_days_depth = max_days_depth
-        
-    def execute(self, context):
-        self.log.info( f"I AM {self.__class__.__name__}" )
-        yesterday = context["yesterday_ds"]
-        day_1 = ds_add(yesterday, -1)
-        self.log.info( f"AVANT HIER {day_1}" )
-        self.log.info( f"MAX DAYS DEPTH {self.max_days_depth}" )
-        for i in range(1, self.max_days_depth):
-            begin = ds_add(yesterday, -i)
-            url_file = self.url_prefix +f"{ ds_format(begin, '%Y-%m-%d', '%Y%m%d')}_to_{ ds_format(yesterday, '%Y-%m-%d', '%Y%m%d')}.tar.gz"
-            self.log.info( f"URL : {url_file}")
-            url = urljoin(self.url_dir, url_file)
-            out = os.path.join(self.out_dir, url_file)
-            try :
-                urllib.request.urlretrieve(url, out)
-            except Exception as e :
-                self.log.info(f"cannot download {url_file} : {e}")
-            else :
-                self.log.info(f"Download {url_file} :  success !!")
-                context["task_instance"].xcom_push(key = "first_date", value = begin)
-                context["task_instance"].xcom_push(key = "last_date", value = yesterday)
-                
-                tar = tarfile.open(out, "r:*")
-                tar_root = os.path.commonprefix(tar.getnames())
-                untar_dir = os.path.join("/tmp/", tar_root)
-                self.log.info( f"TAR DIR : {untar_dir}" )
-                tar.extractall( "/tmp/")
-                tar.close()
-                
-                context["task_instance"].xcom_push(key = "weather_diff_dir", value = untar_dir)
-                break
-        else :
-            self.log.error(f"downloading diff weather files failed : try to increase {self.max_days_depth}")
-            raise(Exception( "downloading diff weather files failed") )
-            
-    
-class dummy_download( BaseOperator):   
-    @apply_defaults
-    def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 url_dir = "https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/superghcnd/",
-                 url_prefix ="superghcnd_diff_",
-                 out_dir="/tmp/",
-                 max_days_depth = 5,
-                 *args, **kwargs):
-
-        super(dummy_download, self).__init__(*args, **kwargs)
-        # Map params here
-        self.url_dir = url_dir
-        self.url_prefix = url_prefix
-        self.out_dir = out_dir
-        self.max_days_depth = max_days_depth
-        
-    def execute(self, context):
-        self.log.info( f"I AM {self.__class_.__name__}" )
-        yesterday = context["yesterday_ds"]
-        day_1 = ds_add(yesterday, -1)
-        self.log.info( f"AVANT HIER {day_1}" )
-        for i in range(1, self.max_days_depth):
-            begin = ds_add(yesterday, -i)
-            url_file = self.url_prefix +f"{ ds_format(begin, '%Y-%m-%d', '%Y%m%d')}_to_{ ds_format(yesterday, '%Y-%m-%d', '%Y%m%d')}.tar.gz"
-            out = os.path.join(self.out_dir, url_file)
-            if os.path.isfile(out) :
-                self.log.info(f"{out} has been donwloaded !!")
-                context["task_instance"].xcom_push(key = "first_date", value = begin)
-                context["task_instance"].xcom_push(key = "last_date", value = yesterday)
-                tar = tarfile.open(out, "r:*")
-                tar_root = os.path.commonprefix(tar.getnames())
-                untar_dir = os.path.join("/tmp/", tar_root)
-                self.log.info( f"TAR DIR : {untar_dir}" )
-                tar.extractall( "/tmp/")
-                tar.close()
-                
-                context["task_instance"].xcom_push(key = "weather_diff_dir", value = untar_dir)
-                break
-        else :
-            self.log.error(f"downloading diff weather files failed : try to increase {self.max_days_depth}")
-            raise(Exception( "downloading diff weather files failed") )
-     
+      
 with DAG(
     dag_id='covid_dag_pouet',
     default_args=args,
@@ -161,28 +64,9 @@ with DAG(
     start_date= datetime(2021,5,2), #days_ago(2), #datetime.datetime.now(), #days_ago(2),
     tags=['covid'],
 ) as dag:
-    
-        #download_diff_weather_task = download_diff_weather(task_id = "download_diff_weather")
- #       download_diff_weather_task = dummy_download(task_id = "download_diff_weather")
-                                                       
+                                                           
         
         download_nyt_task = download_fromweb(task_id = "download_nyt")
-         
-        #process_weather = SparkSubmitOperator(
-            #application="/home/user/CODE/BIG_DATA/CAPSTONE_PROJECT/covid-analysis/airflow/python/transform_weather.py", 
-            #task_id="process_weather",
-            #conn_id = "spark_default",
-            #application_args = [ "{{ ti.xcom_pull( task_ids = 'download_diff_weather', key = 'weather_diff_dir') }}", 
-                                #"/home/user/CODE/BIG_DATA/CAPSTONE_PROJECT/covid-analysis/OUT_DATA/map_locations_stations",
-                                #"/home/user/CODE/BIG_DATA/CAPSTONE_PROJECT/covid-analysis/OUT_DATA/weather_data"]
-
-        #add_weather_task =  add_weather(
-                #task_id = "add_weather",
-                #path_patch_weather = "/tmp/superghcnd_diff_20210420_to_20210421.tar.gz",
-                #path_patch_weather = '/tmp/superghcnd_diff_20210420_to_{{ ds_nodash }}.tar.gz',
-                #db_stations = "/home/user/CODE/BIG_DATA/CAPSTONE_PROJECT/covid-analysis/OUT_DATA/map_locations_stations",
-                #db_weather = "/home/user/CODE/BIG_DATA/CAPSTONE_PROJECT/covid-analysis/OUT_DATA/weather_data"
-                #)
 
         create_recent_per_county_table = PostgresOperator(
             task_id="create_recent_per_county_table",
@@ -331,10 +215,5 @@ with DAG(
             DROP TABLE IF EXISTS recent_per_county;
             """
             )
-       
-     
-
-#download_diff_weather_task >> process_weather
-#download_nyt_task >> process_counties_task
-
+    
 download_nyt_task >> drop_recent_per_county_table >> create_recent_per_county_table >> load_recent_per_county_table  >> add_location_id >> retrieve_past_data >> compute_daily_stats >> filter_date >> append_full_per_county >> update_last_date >> drop_recent_per_county_table_final
