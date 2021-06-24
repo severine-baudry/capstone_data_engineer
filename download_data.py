8240115cefb1df1ac503e6a4a60dd7ca45397bf6
Severine <simone.yvonne.baudry@gmail.com>
2021-06-24 14:50:18 +0200

    use user agent when donwloading weather data, to prevent proxy error (502)

--- download_data.py
+++ download_data.py
@@ -5,12 +5,23 @@ import requests
 from urllib.error import *
 from urllib.parse import urljoin
 import zipfile
+import argparse
+import socket
+import shutil
 
-config = configparser.ConfigParser()
-config.read("capstone.cfg")
 
-os.chdir(config["PATH"]["project"])
-project_path = config["PATH"]["project"]
+def download_url(url, out):
+    agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36"
+    req = urllib.request.Request(url, headers={'User-Agent': agent})
+    print(f"Downloading {url}")                                
+    try :
+         url_bytes = urllib.request.urlopen(req).read()
+    except Exception as inst:
+        print(f"unable to download {url}")
+        raise(inst)
+    else :
+        with open(out, "wb") as fs :
+            fs.write(url_bytes)
 
 
 def download_weather_data(out_dir):
@@ -20,10 +31,10 @@ def download_weather_data(out_dir):
     data_dir = os.path.join(url_dir,"by_year")
     #print(data_dir)
     url_weather = os.path.join( data_dir , weather_2020)
-    #print(url_weather)
     out_weather = os.path.join(out_dir, weather_2020)
     try :
-        urllib.request.urlretrieve(url_weather, out_weather)
+        i=0
+        #download_url(url_weather, out_weather)
     except  Exception as inst:
         print( f"unable to download {url_weather} to {out_weather}" )
         print(inst)
@@ -33,23 +44,34 @@ def download_weather_data(out_dir):
         url = urljoin(url_dir, weather_file)
         out = os.path.join(out_dir,  weather_file)
         try :
-            urllib.request.urlretrieve(url, out)
-        except :
+            download_url(url, out)
+        except Exception as inst:
             print( f"unable to download {url} to {out}" )
-        
+            print(inst)
 def download_covid_percounty(out_dir):
     nyt_dir = "https://github.com/nytimes/covid-19-data/raw/master/"
     nyt_covid  = "us-counties.csv"
     url = urljoin(nyt_dir, nyt_covid)
     out = os.path.join(out_dir, nyt_covid) 
-    urllib.request.urlretrieve(url, out)
+    try :
+        download_url(url, out)
+    except  Exception as inst:
+        print( f"unable to download {url} to {out}" )
+        print(inst)
+        return
     
 def download_gazetteer(out_dir):
     gazetteer_dir = "https://www2.census.gov/geo/docs/maps-data/data/gazetteer/2020_Gazetteer/"
     gazetteer_name ="2020_Gaz_counties_national.zip"
     url = urljoin(gazetteer_dir, gazetteer_name)
     out = os.path.join(out_dir, gazetteer_name)
-    urllib.request.urlretrieve(url, out)
+    print( f"downloading {url}")
+    try :
+        download_url(url, out)
+    except  Exception as inst:
+        print( f"unable to download {url} to {out}" )
+        print(inst)
+        return
     with zipfile.ZipFile(out, 'r') as zip:
         if len( zip.infolist()) != 1 :
             print(f"error :zip does not contain asingle file {len( zip.infolist())}")
@@ -67,8 +89,7 @@ def download_gazetteer(out_dir):
                     i += 1    
         os.remove(uncompress)
         
-def main():
-    out_dir = "DATA2"
+def main(out_dir):
     weather_dir = os.path.join(out_dir, "WEATHER")
     covid_dir = os.path.join( out_dir, "COVID")
     
@@ -88,6 +109,24 @@ def main():
     download_weather_data(weather_dir)
     download_covid_percounty(covid_dir)
     download_gazetteer(out_dir)
+    
+
+def copy_to_hdfs(local_data_dir, hdfs_dir):
+    hostname = socket.gethostname()
+    print( f"hostname : {hostname}")
+    if hostname.startswith(config["HOST"]["PREFIX"]) == False :
+        stream = os.popen( f"hdfs dfs -put {local_data_dir} {hdfs_dir}")
+        res = stream.read()                         
+    
             
 if __name__ == "__main__":
-    main()
+    config = configparser.ConfigParser()
+    config.read("capstone.cfg")
+
+    local_data_dir = config["PATH"]["LOCAL_DOWNLOAD_DIR"]
+    hdfs_dir = config["PATH"]["HDFS_DOWNLOAD_DIR"]
+
+    main(local_data_dir)
+    
+    copy_to_hdfs(local_data_dir,hdfs_dir)
+    
